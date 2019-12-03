397122f005e81547a3c500dace30b966b0b00bf1
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-03 14:31:14 +1000

    Wait time added

--- scripts.js
+++ scripts.js
@@ -1,29 +1,18 @@
 function loaded() {
-    typer("I KNOW YOUR NAME" , 50, "title")
-    typer("Created by Alex Leybourne", 50, "subTitle")
+    typer("I KNOW YOUR NAME" , 50, "title", 0)
+    typer("Created by Alex Leybourne", 50, "subTitle", 1000)
 }
 
 
-// function type(text, speed) {
 
-//     var i = 0
-//     text = "I KNOW YOUR NAME"
-
-//     if (i < text.length) {
-//         document.getElementById("title").innerHTML += text.charAt(i)
-//         i++;
-//         setTimeout(type, speed)
-//     }
-// }
-
-
-function typer(text, speed, id) {
+function typer(text, speed, id, timeout) {
 
     var i = 0;
     var txt = text
     var speed = speed
-    type()
+    
 
+    setTimeout(type, timeout)
     function type() {
     if (i < txt.length) {
         document.getElementById(id).innerHTML += txt.charAt(i);
