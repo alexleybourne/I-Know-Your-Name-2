1ad916a84ebf54b7821a15aabaf918a90ad097ea
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-04 06:56:52 +1000

    Cursor Works

--- scripts.js
+++ scripts.js
@@ -1,41 +1,51 @@
 function loaded() {
     typer("I KNOW YOUR NAME" , 150, "title", 500)
     typer("Created by Alex Leybourne", 100, "subTitle", 4000)
+    typer("Press any Key to begin...", 100, "playText", 8000)
 }
 
+var lastID = ""
+var lastTXT = ""
+var activeC = 1
 
 
 function typer(text, speed, id, timeout) {
 
-    
-
     var i = 0;
     var txt = text
     var newtxt = ""
-    var lastID = ""
-    var lastTXT = ""
-
     var element = document.getElementById(id)
+
     
-    if(lastID != ""){
-            document.getElementById(lastID).innerHTML = lastTXT
-    }
+
+    
+
 
     setTimeout(type, timeout)
     function type() {
-    if (i < txt.length) {
-        element.innerHTML = newtxt
-        element.innerHTML += txt.charAt(i)
-        newtxt = element.innerHTML
-        element.innerHTML += `<p class="cursor">█</p>`
-        i++
-        setTimeout(type, speed)
-    } else {
-        element.innerHTML = newtxt
-        element.innerHTML += `<p class="cursor"> █</p>`
-        lastID = id
-        lastTXT = "POOP"
-    }
+
+        if (activeC === 2){
+            document.getElementById(lastID).innerHTML = lastTXT
+            console.log(lastTXT)
+            console.log(lastID)
+            activeC = 1
+        }
+
+        if (i < txt.length) {
+            element.innerHTML = newtxt
+            element.innerHTML += txt.charAt(i)
+            newtxt = element.innerHTML
+            element.innerHTML += `<p class="cursor">█</p>`
+            i++
+            setTimeout(type, speed)
+        } else {
+            element.innerHTML = newtxt
+            element.innerHTML += `<p class="cursor">█</p>`
+            lastID = id
+            lastTXT = newtxt
+            activeC++
+        }
+    
     }
 
 }
