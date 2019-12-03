4f0fc915b4725a011b31e3066ac5cc2c5243ec7c
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-04 09:39:47 +1000

    Code Insert Function Added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 function loaded() {
+    typer("I KNOW YOUR NAME" , 150, "title", 500, "text")
+    typer("Created by Alex Leybourne", 100, "subTitle", 4000, "text")
+    typer(`<input id="startButton" class="btn pushforward" type="button" style="margin: 60px 0 0 0;" value="START" onclick="start();" />`, 1, "startButtonArea", 8000, "code")
 }
 
 
@@ -8,29 +11,18 @@ var activeC = 1
 var consoleText = document.getElementById("textArea")
 
 function start(){
-    consoleText.innerHTML = `<p id="title" ></p>
-    <p class="small" id="subTitle"></p>
-
-    <p class="small" id="playText" style="padding: 50px 0 0 0;"></p>`
-
-    typer("I KNOW YOUR NAME" , 150, "title", 500)
-    typer("Created by Alex Leybourne", 100, "subTitle", 4000)
-    typer("Press any Key to begin...", 100, "playText", 8000)
+    console.log("Test")
+    location.reload()
 }
 
 
 
-function typer(text, speed, id, timeout) {
+function typer(text, speed, id, timeout, inputType) {
 
     var i = 0;
     var txt = text
     var newtxt = ""
     var element = document.getElementById(id)
-
-    
-
-    
-
 
     setTimeout(type, timeout)
     function type() {
@@ -42,18 +34,26 @@ function typer(text, speed, id, timeout) {
             activeC = 1
         }
 
-        if (i < txt.length) {
-            element.innerHTML = newtxt
-            element.innerHTML += txt.charAt(i)
-            newtxt = element.innerHTML
-            element.innerHTML += `<p class="cursor">█</p>`
-            i++
-            setTimeout(type, speed)
-        } else {
-            element.innerHTML = newtxt
-            element.innerHTML += `<p class="cursor">█</p>`
+        if(inputType === "text") {
+            if (i < txt.length) {
+                element.innerHTML = newtxt
+                element.innerHTML += txt.charAt(i)
+                newtxt = element.innerHTML
+                element.innerHTML += `<p class="cursor">█</p>`
+                i++
+                setTimeout(type, speed)
+            } else {
+                element.innerHTML = newtxt
+                element.innerHTML += `<p class="cursor">█</p>`
+                lastID = id
+                lastTXT = newtxt
+                activeC++
+            }
+        }
+
+        if(inputType === "code") {
+            element.innerHTML = txt
             lastID = id
-            lastTXT = newtxt
             activeC++
         }
     
