a6ad6d23bb0ad8d54678c6987e05fa227318d2e0
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-17 10:42:38 +1000

    Test User Browser

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function loaded() {
-    // typer("EPSTEIN DIDN'T KILL HIMSELF" , 150, "title", 500, "text")
     typer("I KNOW YOUR NAME" , 150, "title", 500, "text")
     typer("Created by Alex Leybourne", 100, "subTitle", 4000, "text")
     typer(`<input id="startButton" class="btn pushforward" type="button" style="margin: 60px 0 0 0;" value="START" onclick="start();" />`, 1, "startButtonArea", 8000, "code")
+
 } 
 // Starting Messages and Button Code 
 //(text to push, what speeed to type, id of where to put the content, how long to wait and if its code or text)
@@ -13,13 +13,14 @@ var lastTXT = ""
 var activeC = 1
 var consoleText = document.getElementById("textArea")
 
+
 // Sounds
 var typingSound = new Audio("./sfx/typing1.wav")
 var beepSound = new Audio("./sfx/beep.wav")
 
-function start(){
+function start() {
     consoleClear()
-    typer("EPSTEIN DIDN'T KILL HIMSELF" , 150, "textMain", 500, "text")
+    browserCheck()
 }
 
 // Clears Text
@@ -78,3 +79,43 @@ function typer(text, speed, id, timeout, inputType) {
     }
     activeC = 1
 }
+
+
+// Test which Browser is being used
+
+function browserCheck() { 
+
+    var userBrowser = ""
+
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+   {
+        userBrowser = 'Opera'
+   }
+   else if(navigator.userAgent.indexOf("Chrome") != -1 )
+   {
+        userBrowser = 'Chrome'
+   }
+   else if(navigator.userAgent.indexOf("Safari") != -1)
+   {
+        userBrowser = 'Safari'
+   }
+   else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+   {
+        userBrowser = 'Firefox'
+   }
+   else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+   {
+        userBrowser ='IE'
+   }  
+   else 
+   {
+        userBrowser = 'unknown'
+   }
+
+   if (userBrowser != 'Chrome') {
+    typer("For the best experience please use Google Chrome" , 150, "textMain", 500, "text")
+   } else {
+    typer("Chrome is the best hey?" , 150, "textMain", 500, "text")
+   }
+
+   }
