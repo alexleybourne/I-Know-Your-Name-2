27643f4862635534f358a7e7f0a746ec2ea05d62
Alex Leybourne <alexleybourne1@gmail.com>
2019-12-25 16:40:52 +1000

    Added sleep function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
-function loaded() {
-    typer("I KNOW YOUR NAME" , 150, "title", 500, "text")
-    typer("Created by Alex Leybourne", 100, "subTitle", 4000, "text")
-    typer(`<input id="startButton" class="btn pushforward" type="button" style="margin: 60px 0 0 0;" value="START" onclick="start();" />`, 1, "startButtonArea", 8000, "code")
+// Sleep Function
+async function sleep(time) {
+    return new Promise(r => setTimeout(r, time))
+}
 
+
+async function loaded() {
+    typer("HE KNOWS WHEN YOU ARE SLEEPING" , 150, "title", 500, "text")
+    await sleep(6000)
+    consoleClear(`<p id="textMain"></p>`)
+    typer("HE KNOWS WHEN YOU ARE AWAKE" , 150, "textMain", 0, "text")
+    // typer("", 100, "subTitle", 4000, "text")
+    // typer(`<input id="startButton" class="btn pushforward" type="button" style="margin: 60px 0 0 0;" value="START" onclick="start();" />`, 1, "startButtonArea", 8000, "code")
+    await sleep(6000)
+    consoleClear(`<p id="textMain" style="font-size: 60px;"></p>`)
+    typer("MERRY CHRISTMAS" , 150, "textMain", 500, "text")
+    // fetch('https://ipapi.co/json/', function(data) {
+    // console.log(JSON.stringify(data, null, 2));
+    // })
 } 
 // Starting Messages and Button Code 
 //(text to push, what speeed to type, id of where to put the content, how long to wait and if its code or text)
@@ -24,8 +38,8 @@ function start() {
 }
 
 // Clears Text
-function consoleClear() {
-    consoleText.innerHTML = `<p id="textMain" ></p>`
+function consoleClear(val) {
+    consoleText.innerHTML = val
 }
 
 function typer(text, speed, id, timeout, inputType) {
@@ -86,8 +100,12 @@ function typer(text, speed, id, timeout, inputType) {
 function browserCheck() { 
 
     var userBrowser = ""
-
-    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    var frameWidth = window.innerWidth
+    
+    if (frameWidth < 700){
+        userBrowser = 'Small'
+    }
+    else if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
    {
         userBrowser = 'Opera'
    }
@@ -112,10 +130,28 @@ function browserCheck() {
         userBrowser = 'unknown'
    }
 
-   if (userBrowser != 'Chrome') {
+    if (userBrowser === 'Small') {
+    typer("For the best experience please use a Computer or Larger Window" , 150, "textMain", 500, "text")
+    } else if (userBrowser != 'Chrome') {
     typer("For the best experience please use Google Chrome" , 150, "textMain", 500, "text")
-   } else {
-    typer("Chrome is the best hey?" , 150, "textMain", 500, "text")
-   }
+    } else {
+    // typer("Chrome is the best hey?" , 150, "textMain", 500, "text")
+    loaded()
+    }
 
-   }
+}
+
+var requestUrl = "http://ip-api.com/json/"
+var ipAddress = ""
+
+fetch('https://www.l2.io/ip', function(data){
+    alert(data)
+})
+
+fetch(requestUrl) // Call the fetch function passing the url of the API as a parameter
+.then(function() {
+    // Your code for handling the data you get from the API
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+})
